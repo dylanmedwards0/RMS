af7ba28f4a1e129808eedcb80c3f65cc7fece43e
dylanmedwards0 <54899115+dylanmedwards0@users.noreply.github.com>
2023-09-11 01:05:20 +0000

    Supabase added

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+
+from flask import Flask, render_template
+from supabase import create_client
+
+url = os.environ['SUPABASE_URL']
+key = os.environ['SUPABASE_KEY']
+supabase = create_client(url, key)
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-    return "Hello, world"
+    return render_template('home.html')
 
 print(__name__)
 if __name__ == "__main__":
